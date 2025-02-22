91a42562d8be81067a3399ae021a5d4f04b4d7b2
arturbozieac <arturbozieac@gmail.com>
2025-02-22 19:03:20 +0200

    project init

---
+++ src/main/java/ro/signsofter/caseobserver/CaseObserverApplication.java
@@ -0,0 +1,13 @@
+package ro.signsofter.caseobserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseObserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseObserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/signsofter/caseobserver/CaseObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.signsofter.caseobserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseObserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
